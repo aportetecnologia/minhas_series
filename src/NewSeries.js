43fbfe748dbbcf4328ff115f424cc831c23a130c
Fábio Barbosa <fabiobarbosa@gmx.com>
2019-02-21 11:52:40 -0300

    Adicionando a lista de Generos no NewSeries.js

--- src/NewSeries.js
+++ src/NewSeries.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react'
+import api from './Api'
+
+const statuses = {
+    'watched': 'Assistido',
+    'watching': 'Assistindo',
+    'toWatch': 'Assistir'
+}
 
 class NewSeries extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            genres: [],
+            isLoading: false
+        }
+    }
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        api.loadGenres()
+            .then((res) => {
+                this.setState({
+                    isLoading: false,
+                    genres: res.data
+                })
+            })
+    }
     render() {
-        return <section className="intro-section"><h1>Nova Série</h1></section>
+        return (
+            <section className="intro-section">
+                <h1>Nova Série</h1>
+                <form>
+                    Nome: <input type="text" className="form-control" /><br />
+                    Status:&nbsp;&nbsp;
+                    <select>
+                        {Object
+                            .keys(statuses)
+                            .map(key => <option key={key} value={key}>{statuses[key]}</option>)
+
+                        }                        
+                    </select><br />
+                    Gênero:&nbsp;&nbsp;
+                    <select>
+                        {this.state.genres
+                            .map(key => <option key={key} value={key}>{key}</option>)
+
+                        }                        
+                    </select><br />
+                    
+                    Comentários: <textarea className="form-control"></textarea><br />
+
+                </form>
+
+
+            </section>
+        )
     }
 }
 export default NewSeries
